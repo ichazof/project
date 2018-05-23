2ca44c985830b839e07c83fa707b6b30450f9af0
ichazof <ichazof@ya.ru>
2018-05-23 23:31:00 +0500

    уу

---
+++ app/src/main/java/com/example/test/start.java
@@ -0,0 +1,20 @@
+package com.example.test;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void click1(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
